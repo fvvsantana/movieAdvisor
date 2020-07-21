795086c8b9e1b2df95bfbde278cce84c3fe4f96d
Flavio Santana <fvvsantana@gmail.com>
2020-07-21 15:22:12 -0300

    Link activities

--- app/src/main/java/com/example/movieadvisor/MainActivity.java
+++ app/src/main/java/com/example/movieadvisor/MainActivity.java
@@ -2,13 +2,34 @@ package com.example.movieadvisor;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
+    public static final String movieIdKey = "movieId";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void goToDetails(View view){
+        Intent intent = new Intent(this, MovieDetailsActivity.class);
+        int movieId = 240;
+        intent.putExtra(movieIdKey, movieId);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/example/movieadvisor/MovieDetailsActivity.java
+++ app/src/main/java/com/example/movieadvisor/MovieDetailsActivity.java
@@ -3,12 +3,24 @@ package com.example.movieadvisor;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MovieDetailsActivity extends AppCompatActivity {
+    private TextView mTvMovieDetails;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_movie_details);
+        mTvMovieDetails = (TextView) findViewById(R.id.tvMovieDetails);
+
+        // Set text view
+        Bundle data = getIntent().getExtras();
+        int movieId;
+        if (data != null) {
+            movieId = data.getInt(MainActivity.movieIdKey);
+            mTvMovieDetails.setText(Integer.toString(movieId));
+        }
+
     }
 }
