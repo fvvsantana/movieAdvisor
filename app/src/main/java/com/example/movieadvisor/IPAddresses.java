00b06758ebba72a7f9789c391e6db978f298acca
Flavio Santana <fvvsantana@gmail.com>
2020-07-21 15:53:25 -0300

    Implement request for the list of movies

---
+++ app/src/main/java/com/example/movieadvisor/IPAddresses.java
@@ -0,0 +1,5 @@
+package com.example.movieadvisor;
+
+public class IPAddresses {
+    public static final String MOVIES_API_URL = "https://desafio-mobile.nyc3.digitaloceanspaces.com/movies";
+}
--- app/src/main/java/com/example/movieadvisor/MainActivity.java
+++ app/src/main/java/com/example/movieadvisor/MainActivity.java
@@ -12,18 +12,53 @@ import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
 
+import org.json.JSONArray;
 import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
     public static final String movieIdKey = "movieId";
+    public final String TAG = getClass().getSimpleName();
+
+    private TextView mTvMovies;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mTvMovies = (TextView) findViewById(R.id.tvMovies);
+
+        // Request movies
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(
+                Request.Method.GET,
+                IPAddresses.MOVIES_API_URL,
+                null,
+                new Response.Listener<JSONArray>() {
+                    @Override
+                    public void onResponse(JSONArray response) {
+                        Log.e(TAG, "Movies response: " + response.toString());
+                        showMovies(response);
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        // TODO: treat errors
+                        Log.e(TAG, "Error on fetching movies: " + error.toString());
+
+                    }
+                }
+        );
+
+        requestQueue.add(jsonArrayRequest);
+    }
+
+    public void showMovies(JSONArray movies){
+        mTvMovies.setText(movies.toString());
     }
 
     public void goToDetails(View view){
