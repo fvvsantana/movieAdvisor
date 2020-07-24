20b6bb4118096999c67816391a197e4684a7130a
Flavio Santana <fvvsantana@gmail.com>
2020-07-24 16:33:12 -0300

    Create class for getting error message from VolleyError

---
+++ app/src/main/java/com/example/movieadvisor/util/VolleyErrorHelper.java
@@ -0,0 +1,55 @@
+package com.example.movieadvisor.util;
+
+import android.content.Context;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.NetworkError;
+import com.android.volley.NetworkResponse;
+import com.android.volley.NoConnectionError;
+import com.android.volley.ServerError;
+import com.android.volley.TimeoutError;
+import com.android.volley.VolleyError;
+import com.example.movieadvisor.R;
+
+public class VolleyErrorHelper {
+
+    // Provide user-readable error message from VolleyError
+    public static String getMessage(VolleyError error, Context context) {
+        if (isNetworkProblem(error)) {
+            return context.getResources().getString(R.string.error_noInternetConnection);
+        }
+        if (isTimeoutProblem(error)) {
+            return context.getResources().getString(R.string.error_timeoutError);
+        }
+        if (isServerProblem(error)) {
+            return handleServerError(error, context);
+        }
+        return context.getResources().getString(R.string.error_genericServerError);
+    }
+
+    // Check if it's a network problem
+    private static boolean isNetworkProblem(VolleyError error) {
+        return (error instanceof NetworkError) || (error instanceof NoConnectionError);
+    }
+
+    // Check if it's a timeout problem
+    private static boolean isTimeoutProblem(VolleyError error) {
+        return error instanceof TimeoutError;
+    }
+
+    // Check if it's a server problem
+    private static boolean isServerProblem(VolleyError error) {
+        return (error instanceof ServerError) || (error instanceof AuthFailureError);
+    }
+
+    // Generate string for server error
+    private static String handleServerError(VolleyError error, Context context) {
+        NetworkResponse response = error.networkResponse;
+
+        if (response != null) {
+            return context.getResources()
+                    .getString(R.string.error_genericServerError + ' ' +  response.statusCode);
+        }
+        return context.getResources().getString(R.string.error_genericServerError);
+    }
+}
