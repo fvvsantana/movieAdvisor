fdae7db265904b202e2ec99b00ce7a6cf135b647
Flavio Santana <fvvsantana@gmail.com>
2020-07-26 15:11:16 -0300

    Use a fragment for the error handling in the MainActivity

--- app/src/main/java/com/example/movieadvisor/fragments/ErrorFragment.java
+++ app/src/main/java/com/example/movieadvisor/fragments/ErrorFragment.java
@@ -7,13 +7,18 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.FrameLayout;
 import android.widget.TextView;
 
 import com.example.movieadvisor.R;
 
 public class ErrorFragment extends Fragment {
 
+    private View mFragmentView;
     private TextView mTvError;
+    private Button mBtnRetry;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,17 +29,24 @@ public class ErrorFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View view = inflater.inflate(R.layout.fragment_error, container, false);
-        mTvError =  view.findViewById(R.id.fragment_error_tvError);
-        return view;
+        mFragmentView = inflater.inflate(R.layout.fragment_error, container, false);
+        mTvError =  mFragmentView.findViewById(R.id.fragment_error_tvError);
+        mBtnRetry = mFragmentView.findViewById(R.id.fragment_error_btnRetry);
+        return mFragmentView;
     }
 
-    public void setErrorMessage(String errorMessage){
+    public void show(String errorMessage){
         mTvError.setText(errorMessage);
+        mFragmentView.setVisibility(View.VISIBLE);
+    }
+
+    public void remove(){
+        mFragmentView.setVisibility(View.GONE);
     }
 
 
-    public void setButtonOnClickListener(View.OnClickListener listener){
-        // TODO: Implement this method
+
+    public void setRetryButtonOnClickListener(View.OnClickListener listener){
+        mBtnRetry.setOnClickListener(listener);
     }
 }
